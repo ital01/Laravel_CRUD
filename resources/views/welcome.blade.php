014127e73a158733ca0717ea589f59bc8349fd88
ital01 <italolenza@gmail.com>
2024-02-19 09:06:52 -0300

    organizacao

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,13 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Cadastro</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
 <body>
-
 <section class="container">
     @if (Route::has('login'))
         <div class="fixed-top p-4 text-right">
@@ -60,23 +57,16 @@
         </form>
     </div>
 
-    <div class="row">
-        <div class="w-75">
-            <div class="w-75">
-                <div class="w-75">
-                    <div class="col-md-12 w-75">
-                        <form action="{{ route('search') }}" method="GET" class="mb-4">
-                            <div class="input-group">
-                                <input type="text" name="search" class="form-control" placeholder="Pesquisa por ID, Nome, ou Email">
-                                <button type="submit" class="btn btn-primary">PROCURAR</button>
-                            </div>
-                        </form>
-                    </div>
+    <div style="width: 30%;">
+        <div class="col-md-12">
+            <form action="{{ route('search') }}" method="GET" class="mb-4">
+                <div class="input-group">
+                    <input type="text" name="search" class="form-control" placeholder="Pesquisa por ID, Nome, ou Email">
+                    <button type="submit" class="btn btn-primary">PROCURAR</button>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
-
 
     <table class="table table-bordered table-striped">
 
@@ -170,10 +160,7 @@
 
 
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
-<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
-    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 
 </html>
