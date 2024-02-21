02009ae840e6e0efda60dfbcb429d84b47b2fb3e
ital01 <italolenza@gmail.com>
2024-02-21 13:04:01 -0300

    button dashboard estilo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,16 +1,11 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
 
-        <!-- Hamburger -->
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Navigation Links -->
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item fs-5">
-                    <a class="nav-link text-white" href="{{ route('dashboard') }}">{{ __('Dashboard') }}</a>
+                    <button class="btn btn-outline-light fs-5" onclick="window.location='{{ route('dashboard') }}'">{{ __('Dashboard') }}</button>
                 </li>
             </ul>
 
