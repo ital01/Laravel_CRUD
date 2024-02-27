08c86a4315c729673ac9f34e41cf83e1a456ff05
ital01 <italolenza@gmail.com>
2024-02-27 09:32:31 -0300

    data table

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
     <title>Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/2.0.0/css/dataTables.bootstrap5.min.css">
 </head>
 
 <body class="bg-light">
@@ -111,8 +111,10 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
-    <script type="text/javascript" charset="utf8"
-        src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+        <script type="text/javascript" charset="utf8"
+        src="https://cdn.datatables.net/2.0.0/js/dataTables.min.js"></script>
+        <script type="text/javascript" charset="utf8"
+        src="https://cdn.datatables.net/2.0.0/js/dataTables.bootstrap5.min.js"></script>
         <script>
             document.addEventListener('DOMContentLoaded', function () {
                 const editarButtons = document.querySelectorAll('.editar-btn');
